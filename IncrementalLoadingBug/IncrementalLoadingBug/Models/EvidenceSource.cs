c3852f4cc08199b6116122ae88f859bad1d4a88b
Touseef Malik <Touseef@geninf.com>
2019-08-27 12:38:41 +0500

    tried a soluton but it didnt work

--- IncrementalLoadingBug/IncrementalLoadingBug/Models/EvidenceSource.cs
+++ IncrementalLoadingBug/IncrementalLoadingBug/Models/EvidenceSource.cs
@@ -1,6 +1,6 @@
-﻿using IncrementalLoadingBug.ViewModels;
-using Microsoft.Toolkit.Collections;
+﻿using Microsoft.Toolkit.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,14 +8,22 @@ namespace IncrementalLoadingBug.Models
 {
     public class EvidencesSource : IIncrementalSource<EvidenceDTO>
     {
-        public AppViewModel AppVM => App.AppVM;
+        private readonly List<EvidenceDTO> evidences;
+
+        public EvidencesSource()
+        {
+            evidences = new List<EvidenceDTO>();
+            for (int i = 0; i < 40; i++)
+            {
+                evidences.Add(new EvidenceDTO());
+            }
+        }
 
         public async Task<IEnumerable<EvidenceDTO>> GetPagedItemsAsync(int pageIndex, int pageSize, CancellationToken cancellationToken = default)
-        {
-            var evidences = new List<EvidenceDTO>();
+        {            
             await Task.Delay(500); // fake web api call
-            evidences.Add(new EvidenceDTO ());
-            return evidences;
+            return (from dto in evidences
+                          select dto).Skip(pageIndex * pageSize).Take(pageSize);
         }
     }
 }
